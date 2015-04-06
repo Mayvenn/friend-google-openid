fda0b053a7049e7276abceb68cd99450269b6506
Sheel Choksi <sheelchoksi@gmail.com>
2015-04-06 11:19:13 -0700

    Update descs and deps

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
 # friend-google-openid
 
-A Clojure library designed to ... well, that part is up to you.
-
-## Usage
-
-FIXME
-
-## License
-
-Copyright © 2015 FIXME
-
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+This is a workflow for Friend that handles Google's OpenID Connect.
--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject friend-google-openid "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Friend workflow for Google OpenID Connect"
+  :url "http://github.com/Mayvenn/friend-google-openid"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.cemerick/friend "0.2.1"]
+                 [friend-oauth2 "0.1.3"]
+                 [com.google.api-client/google-api-client "1.20.0"]])

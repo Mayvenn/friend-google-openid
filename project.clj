f2e5fe3196745196d38413abfc63e2f6c1cc7a18
Sheel Choksi <sheelchoksi@gmail.com>
2015-04-08 12:15:45 -0700

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject friend-google-openid "0.2.0-SNAPSHOT"
+(defproject friend-google-openid "0.2.0"
   :description "Friend workflow for Google OpenID Connect"
   :url "http://github.com/Mayvenn/friend-google-openid"
   :license {:name "Eclipse Public License"
